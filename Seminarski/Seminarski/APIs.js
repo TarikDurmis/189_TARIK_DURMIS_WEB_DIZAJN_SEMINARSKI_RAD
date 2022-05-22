cbcf73c205704e2658f733bc670475957e287e0c
TarikDurmis <97351178+TarikDurmis@users.noreply.github.com>
2022-05-22 19:03:01 +0200

    Add files via upload
    meni i get API

---
+++ Seminarski/Seminarski/APIs.js
@@ -0,0 +1,31 @@
+const app = document.getElementById('base');
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(container);
+
+async function catchJson(){
+
+    const response = await fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Food');
+    const food = await response.json();
+    
+    food.forEach(Food => {
+        const box = document.createElement('div')
+        box.setAttribute('class', 'box')
+
+        const h1 = document.createElement('h1')
+        h1.textContent = Food.name;
+
+        const p = document.createElement('p');
+        p.textContent = Food.price + " $";
+        container.appendChild(box);
+        
+        const image = document.createElement('img');
+        image.src = Food.imageUrl;
+
+        box.appendChild(h1);
+        box.appendChild(image);
+        box.appendChild(p)
+        });
+}
